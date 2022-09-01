54983daf2f0bd682b7f8d3ab8625ea907a33c350
mamahara <er.maharana@gmail.com>
2022-09-02 02:54:59 +0400

    fixing POM

--- src/main/java/com/tacx/assignment/WebApplication.java
+++ src/main/java/com/tacx/assignment/WebApplication.java
@@ -2,10 +2,13 @@ package com.tacx.assignment;
 
 import io.jaegertracing.Configuration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
