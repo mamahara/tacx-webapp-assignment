7d6baf97ed7a71dece9e55ebf35c429305580709
mamahara <er.maharana@gmail.com>
2022-09-03 15:16:02 +0400

    Added workflows

--- terraform/modules/tacx-webapp-assignment/deploy.tf
+++ terraform/modules/tacx-webapp-assignment/deploy.tf
@@ -1,4 +1,4 @@
-#  Deploy code from GitHub repo
+#  Deploy code from GitHub repository
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id             = azurerm_linux_web_app.webapp.id
   repo_url           = "https://github.com/mamahara/tacx-webapp-assignment"
