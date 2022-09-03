28040ea583caec9210e64b2573958b78da9300c2
mamahara <er.maharana@gmail.com>
2022-09-03 16:41:45 +0400

    Added workflows

--- terraform/modules/tacx-webapp-assignment/rg.tf
+++ terraform/modules/tacx-webapp-assignment/rg.tf
@@ -1,6 +1,6 @@
 #---------------------------------------------------------
 # Resource Group Creation or selection - Default is "true"
-#----------------------------------------------------------
+#----------------------------------------------------------#
 resource "azurerm_resource_group" "rg" {
   count    = var.create_resource_group ? 1 : 0
   name     = var.resource_group_name
